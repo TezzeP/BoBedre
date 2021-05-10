00378a1a69cbfcddf051ac054fe2ceeeed13ecb7
TezzeP <64412045+TezzeP@users.noreply.github.com>
2021-05-10 11:20:08 +0200

    jadlas

--- BoBedreVS/CRUD/OpdaterBolig.Designer.cs
+++ BoBedreVS/CRUD/OpdaterBolig.Designer.cs
@@ -94,10 +94,10 @@ namespace BoBedreVS
             // Update
             // 
             this.Update.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Update.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Update.Location = new System.Drawing.Point(203, 407);
+            this.Update.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Update.Location = new System.Drawing.Point(749, 15);
             this.Update.Name = "Update";
-            this.Update.Size = new System.Drawing.Size(85, 41);
+            this.Update.Size = new System.Drawing.Size(77, 33);
             this.Update.TabIndex = 1;
             this.Update.Text = "Update";
             this.Update.UseVisualStyleBackColor = true;
@@ -114,7 +114,7 @@ namespace BoBedreVS
             // 
             // BoligAdresseTextBox
             // 
-            this.BoligAdresseTextBox.Location = new System.Drawing.Point(298, 80);
+            this.BoligAdresseTextBox.Location = new System.Drawing.Point(339, 80);
             this.BoligAdresseTextBox.Mask = "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL";
             this.BoligAdresseTextBox.Name = "BoligAdresseTextBox";
             this.BoligAdresseTextBox.Size = new System.Drawing.Size(125, 23);
@@ -122,7 +122,7 @@ namespace BoBedreVS
             // 
             // BoligPostNrTextBox
             // 
-            this.BoligPostNrTextBox.Location = new System.Drawing.Point(298, 125);
+            this.BoligPostNrTextBox.Location = new System.Drawing.Point(339, 125);
             this.BoligPostNrTextBox.Mask = "0000";
             this.BoligPostNrTextBox.Name = "BoligPostNrTextBox";
             this.BoligPostNrTextBox.Size = new System.Drawing.Size(125, 23);
@@ -130,7 +130,7 @@ namespace BoBedreVS
             // 
             // KundeIdFKTextBox
             // 
-            this.KundeIdFKTextBox.Location = new System.Drawing.Point(587, 379);
+            this.KundeIdFKTextBox.Location = new System.Drawing.Point(701, 379);
             this.KundeIdFKTextBox.Mask = "00000";
             this.KundeIdFKTextBox.Name = "KundeIdFKTextBox";
             this.KundeIdFKTextBox.Size = new System.Drawing.Size(125, 23);
@@ -138,7 +138,7 @@ namespace BoBedreVS
             // 
             // BoligGrundM2TextBox
             // 
-            this.BoligGrundM2TextBox.Location = new System.Drawing.Point(298, 167);
+            this.BoligGrundM2TextBox.Location = new System.Drawing.Point(339, 167);
             this.BoligGrundM2TextBox.Mask = "00000";
             this.BoligGrundM2TextBox.Name = "BoligGrundM2TextBox";
             this.BoligGrundM2TextBox.Size = new System.Drawing.Size(125, 23);
@@ -146,7 +146,7 @@ namespace BoBedreVS
             // 
             // BoigHusM2TextBox
             // 
-            this.BoigHusM2TextBox.Location = new System.Drawing.Point(298, 210);
+            this.BoigHusM2TextBox.Location = new System.Drawing.Point(339, 210);
             this.BoigHusM2TextBox.Mask = "00000";
             this.BoigHusM2TextBox.Name = "BoigHusM2TextBox";
             this.BoigHusM2TextBox.Size = new System.Drawing.Size(125, 23);
@@ -154,7 +154,7 @@ namespace BoBedreVS
             // 
             // BoligPrisTextBox
             // 
-            this.BoligPrisTextBox.Location = new System.Drawing.Point(298, 254);
+            this.BoligPrisTextBox.Location = new System.Drawing.Point(339, 254);
             this.BoligPrisTextBox.Mask = "000000000";
             this.BoligPrisTextBox.Name = "BoligPrisTextBox";
             this.BoligPrisTextBox.Size = new System.Drawing.Size(125, 23);
@@ -163,7 +163,7 @@ namespace BoBedreVS
             // 
             // BoligSalgsdatoTextBox
             // 
-            this.BoligSalgsdatoTextBox.Location = new System.Drawing.Point(587, 201);
+            this.BoligSalgsdatoTextBox.Location = new System.Drawing.Point(701, 201);
             this.BoligSalgsdatoTextBox.Mask = "00/00/0000";
             this.BoligSalgsdatoTextBox.Name = "BoligSalgsdatoTextBox";
             this.BoligSalgsdatoTextBox.Size = new System.Drawing.Size(125, 23);
@@ -171,7 +171,7 @@ namespace BoBedreVS
             // 
             // BoligOprettelsesTextBox
             // 
-            this.BoligOprettelsesTextBox.Location = new System.Drawing.Point(587, 246);
+            this.BoligOprettelsesTextBox.Location = new System.Drawing.Point(701, 246);
             this.BoligOprettelsesTextBox.Mask = "00/00/0000";
             this.BoligOprettelsesTextBox.Name = "BoligOprettelsesTextBox";
             this.BoligOprettelsesTextBox.Size = new System.Drawing.Size(125, 23);
@@ -180,7 +180,7 @@ namespace BoBedreVS
             // 
             // StandSkalaTextBox
             // 
-            this.StandSkalaTextBox.Location = new System.Drawing.Point(587, 336);
+            this.StandSkalaTextBox.Location = new System.Drawing.Point(701, 336);
             this.StandSkalaTextBox.Mask = "000000000";
             this.StandSkalaTextBox.Name = "StandSkalaTextBox";
             this.StandSkalaTextBox.Size = new System.Drawing.Size(125, 23);
@@ -260,7 +260,7 @@ namespace BoBedreVS
             // 
             this.label7.AutoSize = true;
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label7.Location = new System.Drawing.Point(468, 204);
+            this.label7.Location = new System.Drawing.Point(544, 204);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(59, 15);
             this.label7.TabIndex = 19;
@@ -270,7 +270,7 @@ namespace BoBedreVS
             // 
             this.label8.AutoSize = true;
             this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label8.Location = new System.Drawing.Point(468, 249);
+            this.label8.Location = new System.Drawing.Point(544, 249);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(91, 15);
             this.label8.TabIndex = 20;
@@ -280,7 +280,7 @@ namespace BoBedreVS
             // 
             this.label9.AutoSize = true;
             this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label9.Location = new System.Drawing.Point(468, 339);
+            this.label9.Location = new System.Drawing.Point(544, 339);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(93, 15);
             this.label9.TabIndex = 21;
@@ -290,7 +290,7 @@ namespace BoBedreVS
             // 
             this.label10.AutoSize = true;
             this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label10.Location = new System.Drawing.Point(468, 383);
+            this.label10.Location = new System.Drawing.Point(544, 383);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(70, 15);
             this.label10.TabIndex = 22;
@@ -308,7 +308,7 @@ namespace BoBedreVS
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(186, 460);
+            this.panel1.Size = new System.Drawing.Size(186, 437);
             this.panel1.TabIndex = 23;
             // 
             // MedarbejderPortal
@@ -443,7 +443,7 @@ namespace BoBedreVS
             // 
             this.label14.AutoSize = true;
             this.label14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label14.Location = new System.Drawing.Point(468, 128);
+            this.label14.Location = new System.Drawing.Point(544, 128);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(77, 15);
             this.label14.TabIndex = 25;
@@ -453,7 +453,7 @@ namespace BoBedreVS
             // 
             this.label15.AutoSize = true;
             this.label15.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label15.Location = new System.Drawing.Point(468, 166);
+            this.label15.Location = new System.Drawing.Point(544, 166);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(106, 15);
             this.label15.TabIndex = 26;
@@ -463,7 +463,7 @@ namespace BoBedreVS
             // 
             this.label16.AutoSize = true;
             this.label16.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label16.Location = new System.Drawing.Point(468, 83);
+            this.label16.Location = new System.Drawing.Point(544, 83);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(40, 15);
             this.label16.TabIndex = 27;
@@ -491,7 +491,7 @@ namespace BoBedreVS
             // 
             // BadAlderTextBox1
             // 
-            this.BadAlderTextBox1.Location = new System.Drawing.Point(587, 163);
+            this.BadAlderTextBox1.Location = new System.Drawing.Point(701, 163);
             this.BadAlderTextBox1.Mask = "00/00/0000";
             this.BadAlderTextBox1.Name = "BadAlderTextBox1";
             this.BadAlderTextBox1.Size = new System.Drawing.Size(125, 23);
@@ -500,7 +500,7 @@ namespace BoBedreVS
             // 
             // KøkkenAlderTextBox
             // 
-            this.KøkkenAlderTextBox.Location = new System.Drawing.Point(587, 125);
+            this.KøkkenAlderTextBox.Location = new System.Drawing.Point(701, 125);
             this.KøkkenAlderTextBox.Mask = "00/00/0000";
             this.KøkkenAlderTextBox.Name = "KøkkenAlderTextBox";
             this.KøkkenAlderTextBox.Size = new System.Drawing.Size(125, 23);
@@ -509,7 +509,7 @@ namespace BoBedreVS
             // 
             // EtagerTextBox
             // 
-            this.EtagerTextBox.Location = new System.Drawing.Point(587, 80);
+            this.EtagerTextBox.Location = new System.Drawing.Point(701, 80);
             this.EtagerTextBox.Mask = "00000";
             this.EtagerTextBox.Name = "EtagerTextBox";
             this.EtagerTextBox.Size = new System.Drawing.Size(125, 23);
@@ -517,7 +517,7 @@ namespace BoBedreVS
             // 
             // AlderTextBox
             // 
-            this.AlderTextBox.Location = new System.Drawing.Point(298, 375);
+            this.AlderTextBox.Location = new System.Drawing.Point(339, 375);
             this.AlderTextBox.Mask = "00000";
             this.AlderTextBox.Name = "AlderTextBox";
             this.AlderTextBox.Size = new System.Drawing.Size(125, 23);
@@ -525,7 +525,7 @@ namespace BoBedreVS
             // 
             // HaveTextBox
             // 
-            this.HaveTextBox.Location = new System.Drawing.Point(298, 334);
+            this.HaveTextBox.Location = new System.Drawing.Point(339, 334);
             this.HaveTextBox.Mask = "0000";
             this.HaveTextBox.Name = "HaveTextBox";
             this.HaveTextBox.Size = new System.Drawing.Size(125, 23);
@@ -533,7 +533,7 @@ namespace BoBedreVS
             // 
             // EjendomsTypeTextBox
             // 
-            this.EjendomsTypeTextBox.Location = new System.Drawing.Point(298, 293);
+            this.EjendomsTypeTextBox.Location = new System.Drawing.Point(339, 293);
             this.EjendomsTypeTextBox.Mask = "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL";
             this.EjendomsTypeTextBox.Name = "EjendomsTypeTextBox";
             this.EjendomsTypeTextBox.Size = new System.Drawing.Size(125, 23);
@@ -543,7 +543,7 @@ namespace BoBedreVS
             // 
             this.label19.AutoSize = true;
             this.label19.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(240)))));
-            this.label19.Location = new System.Drawing.Point(468, 293);
+            this.label19.Location = new System.Drawing.Point(544, 293);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(91, 15);
             this.label19.TabIndex = 36;
@@ -551,7 +551,7 @@ namespace BoBedreVS
             // 
             // OmbygningsÅrTextBox
             // 
-            this.OmbygningsÅrTextBox.Location = new System.Drawing.Point(587, 290);
+            this.OmbygningsÅrTextBox.Location = new System.Drawing.Point(701, 290);
             this.OmbygningsÅrTextBox.Mask = "00/00/0000";
             this.OmbygningsÅrTextBox.Name = "OmbygningsÅrTextBox";
             this.OmbygningsÅrTextBox.Size = new System.Drawing.Size(125, 23);
@@ -563,7 +563,7 @@ namespace BoBedreVS
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(782, 460);
+            this.ClientSize = new System.Drawing.Size(859, 437);
             this.Controls.Add(this.OmbygningsÅrTextBox);
             this.Controls.Add(this.label19);
             this.Controls.Add(this.EjendomsTypeTextBox);
